1d5945a3c56370f30e2e8fa2d1b1b446c5f68813
gsengupta2810 <gsengupta2810@gmail.com>
2016-05-24 15:31:34 +0530

    main changed

--- src/pmain.cpp
+++ src/pmain.cpp
@@ -11,16 +11,22 @@
 #include <tactics/tactic_factory.h>
 #include <string>
 #include <utility>
+#include <fstream>
 
 using namespace krssg_ssl_msgs;
+using namespace Strategy;
 
 ros::Subscriber state_sub;
 krssg_ssl_msgs::BeliefState state;
 std::vector<std::pair<std::string, Strategy::Tactic::Param> > roleList;
-unsigned int currTacticIdx;
+
+void publishing();
 
 void Callback(const krssg_ssl_msgs::BeliefState::ConstPtr& msg)
 {
+    
+  ROS_INFO("in Callback %f \n",msg->ballPos.x);
+
   state.isteamyellow=msg->isteamyellow;
   state.frame_number=msg->frame_number ;
   state.t_capture=msg->t_capture  ;   
@@ -32,21 +38,13 @@ void Callback(const krssg_ssl_msgs::BeliefState::ConstPtr& msg)
   state.ballDetected=msg->ballDetected;
   state.homeDetected=msg->homeDetected;
   state.awayDetected=msg->awayDetected;
+  if(1)
+  publishing();
   return;
 }
 
-void publish()
+void publishing()
 {
-  
-}
-
-int main(int argc, char  *argv[])
-{
-  // send a dummy TacticPacket
-  using namespace Strategy;
-  ros::init(argc, argv, "play_main_node");
-  ros::NodeHandle n1;
-  state_sub = n1.subscribe("/belief_state", 1000, Callback);
   ros::NodeHandle n;
   ros::Publisher tp0_pub = n.advertise<krssg_ssl_msgs::TacticPacket>("tactic_0", 1000);
   ros::Publisher tp1_pub = n.advertise<krssg_ssl_msgs::TacticPacket>("tactic_1", 1000);
@@ -57,65 +55,70 @@ int main(int argc, char  *argv[])
 
   //****************************************************************
   krssg_ssl_msgs::TacticPacket tp0, tp1,tp2,tp3,tp4,tp5;
-  ros::Rate loop_rate(100);
-  PExec pExec(&state,n);
   //****************************************************************
 
-  while (ros::ok())
-    {
-        
-        if(pExec.playTerminated())
-       {
-         // Util::Logger::toStdOut("*************Play terminated.Select new play\n*********************");
-          pExec.evaluatePlay();
-          pExec.selectPlay();
-        }
-        Robot** robot=pExec.executePlay();
-        
-        Tactic::Param tParam;
+   PExec pExec(&state,n); 
+  if(pExec.playTerminated())
+ {
 
-        tp0.tID = std::string(robot[0]->tID);
-        //tParam=roleList[0][currTacticIdx].second;
-        //tp0.tParamJSON = TacticFactory::instance()->Create(roleList[0][currTacticIdx], 0)->paramToJSON(tParam); // bot id really doesnt matter here
-        tp0.tParamJSON =robot[0]->tParamJSON;
-
-        tp1.tID = std::string(robot[1]->tID);
-        //tParam=roleList[1][currTacticIdx].second;
-        //tp1.tParamJSON = TacticFactory::instance()->Create(roleList[1], 1)->paramToJSON(tParam); // bot id really doesnt matter here
-        tp0.tParamJSON =robot[1]->tParamJSON;
-        
-        tp2.tID = std::string(robot[2]->tID);
-        //tParam=roleList[2][currTacticIdx].second;
-        //tp2.tParamJSON = TacticFactory::instance()->Create(roleList[2], 2)->paramToJSON(tParam); // bot id really doesnt matter here
-        tp0.tParamJSON =robot[2]->tParamJSON;
-        
-        tp3.tID = std::string(robot[3]->tID);
-        //tParam=roleList[3][currTacticIdx].second;
-        //tp3.tParamJSON = TacticFactory::instance()->Create(roleList[3], 3)->paramToJSON(tParam); // bot id really doesnt matter here
-        tp0.tParamJSON =robot[3]->tParamJSON;
-        
-        tp4.tID = std::string(robot[4]->tID);
-        //tParam=roleList[4][currTacticIdx].second;
-        //tp4.tParamJSON = TacticFactory::instance()->Create(roleList[4], 0)->paramToJSON(tParam); // bot id really doesnt matter here
-        tp0.tParamJSON =robot[0]->tParamJSON;
-        
-        tp5.tID = std::string(robot[5]->tID);
-        //tParam=roleList[0][currTacticIdx].second;
-        //tp5.tParamJSON = TacticFactory::instance()->Create(roleList[5], 0)->paramToJSON(tParam); // bot id really doesnt matter here
-        tp0.tParamJSON =robot[5]->tParamJSON;
-
-        tp0_pub.publish(tp0);
-        tp1_pub.publish(tp1);
-        tp2_pub.publish(tp2);
-        tp3_pub.publish(tp3);
-        tp4_pub.publish(tp4);
-        tp5_pub.publish(tp5);
-
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-
-  //****************************************************************
+   // Util::Logger::toStdOut("*************Play terminated.Select new play\n*********************");
+    pExec.evaluatePlay();
+    pExec.selectPlay();
+  }
+  Robot** robot=pExec.executePlay();
   
+  Tactic::Param tParam;
+
+  tp0.tID = std::string(robot[0]->tID);
+  //ROS_INFO("in Callback %s \n",tp0.tID);
+  //tParam=roleList[0][currTacticIdx].second;
+  //tp0.tParamJSON = TacticFactory::instance()->Create(roleList[0][currTacticIdx], 0)->paramToJSON(tParam); // bot id really doesnt matter here
+  tp0.tParamJSON =robot[0]->tParamJSON;
+
+  tp1.tID = std::string(robot[1]->tID);
+  //ROS_INFO("in Callback %c \n",tp1.tID);
+  //tParam=roleList[1][currTacticIdx].second;
+  //tp1.tParamJSON = TacticFactory::instance()->Create(roleList[1], 1)->paramToJSON(tParam); // bot id really doesnt matter here
+  tp0.tParamJSON =robot[1]->tParamJSON;
+  
+  tp2.tID = std::string(robot[2]->tID);
+  //ROS_INFO("in Callback %c \n",tp2.tID);
+  //tParam=roleList[2][currTacticIdx].second;
+  //tp2.tParamJSON = TacticFactory::instance()->Create(roleList[2], 2)->paramToJSON(tParam); // bot id really doesnt matter here
+  tp0.tParamJSON =robot[2]->tParamJSON;
+  
+  tp3.tID = std::string(robot[3]->tID);
+  //ROS_INFO("in Callback %c \n",tp3.tID);
+  //tParam=roleList[3][currTacticIdx].second;
+  //tp3.tParamJSON = TacticFactory::instance()->Create(roleList[3], 3)->paramToJSON(tParam); // bot id really doesnt matter here
+  tp0.tParamJSON =robot[3]->tParamJSON;
+  
+  tp4.tID = std::string(robot[4]->tID);
+  //ROS_INFO("in Callback %c \n",tp4.tID);
+  //tParam=roleList[4][currTacticIdx].second;
+  //tp4.tParamJSON = TacticFactory::instance()->Create(roleList[4], 0)->paramToJSON(tParam); // bot id really doesnt matter here
+  tp0.tParamJSON =robot[0]->tParamJSON;
+  
+  tp5.tID = std::string(robot[5]->tID);
+  //ROS_INFO("in Callback %c \n",tp5.tID);
+  //tParam=roleList[0][currTacticIdx].second;
+  //tp5.tParamJSON = TacticFactory::instance()->Create(roleList[5], 0)->paramToJSON(tParam); // bot id really doesnt matter here
+  tp0.tParamJSON =robot[5]->tParamJSON;
+
+  tp0_pub.publish(tp0);
+  tp1_pub.publish(tp1);
+  tp2_pub.publish(tp2);
+  tp3_pub.publish(tp3);
+  tp4_pub.publish(tp4);
+  tp5_pub.publish(tp5);  
+}
+
+int main(int argc, char  *argv[])
+{
+  // send a dummy TacticPacket
+  ros::init(argc, argv, "play_node");
+  ros::NodeHandle n;
+  state_sub = n.subscribe("/belief_state", 1000, Callback);
+  ros::spin();
   return 0;
 }
